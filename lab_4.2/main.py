ca6710526039f296ca249eba7d6f4af32edd1dcc
PopanDoss <chernenko.seroja@gmail.com>
2024-02-05 19:46:49 +0200

    Переписав використовуючи split(',')

--- lab_4.2/main.py
+++ lab_4.2/main.py
@@ -2,27 +2,31 @@ from  pathlib import Path
 import os 
 
 def get_cats_info(path):
-
+    
     cat_list = [] 
-    #Перевіряємо шлях до файлу 
-    if os.path.exists(path):
+    
+    try:
+        #Перевіряємо шлях до файлу 
+        if os.path.exists(path):
         
-        with open( path, 'r+') as base:
-            raw_list = [el.strip() for el in base.readlines()]
+            with open( path, 'r+') as base:
+                raw_list = [el.strip() for el in base.readlines()]
 
-        #перевіряємо чи файл пустий 
-        if len(raw_list) > 0:
+            #перевіряємо чи файл пустий 
+            if len(raw_list) > 0:
             
-            #Створюємо список словників з даних в файлі
-            for i in raw_list:
-                cat = i.split(',') 
-                cat_list.append({'id': cat[0], 'name': cat[1], 'age': cat[2]})
+                #Створюємо список словників з даних в файлі
+                for i in raw_list:
+                    cat = i.split(',') 
+                    cat_list.append({'id': cat[0], 'name': cat[1], 'age': cat[2]})
 
-            return cat_list
+                return cat_list
+            else:
+                return 'Перевірте всім файлу'
         else:
-            return 'Перевірте всім файлу'
-    else:
-        return 'Перевірте шлях до файлу'
+            return 'Перевірте шлях до файлу'
+    except:
+        print('Виникла помилка')
     
         
 
